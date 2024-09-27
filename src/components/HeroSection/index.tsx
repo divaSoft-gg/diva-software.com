b9f342297e6d9b914c18767c8f91ef4b8aabec8f
aymkh <aymen4real@gmail.com>
2024-09-27 14:52:08 +0100

    chore: minor changes

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -7,22 +7,18 @@ const HeroSection = () => {
 
     return (
         <CentredLayout>
-            <section className="flex flex-row justify-between gap-8 dark">
-                <div className="">
-                    <h1 className="max-w-2xl mb-4 text-6xl font-extrabold tracking-tight text-center text-black dark:text-white">
-                        Payments tool for software companies
+            <section className="flex flex-row justify-between gap-8 my-16 dark">
+                <div className="flex flex-col gap-8">
+                    <h1 className="max-w-2xl mb-4 text-6xl font-semibold tracking-tight text-left text-black dark:text-white">
+                        Design. Develop. Deliver.
                     </h1>
-                    <p className="max-w-2xl mb-6 text-lg font-light text-center text-gray-300 ">
-                        From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
+                    <p className="max-w-2xl mb-6 text-lg font-light text-left text-gray-300 ">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia vero officiis explicabo accusamus eius impedit soluta doloremque mollitia veniam officia, quam unde omnis, ipsum incidunt dolorum sint sapiente minima perferendis at fuga dolor natus! Aperiam molestiae ut ipsa nisi praesentium.
                     </p>
-                    <div className="flex justify-center ">
 
-                        <Button variant="shadow" color="danger" radius="lg" >Get Started</Button>
-                        <Spacer x={4} />
-                        <Button variant="shadow" color="primary" radius="lg" >Contact Us</Button>
-                    </div>
+                    <Button variant="flat" color="warning" radius="lg" size="lg" className="w-fit">Contact Us</Button>
                 </div>
-                <Image src="images/hero-section/placeholder.jpg" className="" width={400} />
+                <Image src="images/hero-section/placeholder.jpg" width={400} />
             </section>
         </CentredLayout>
     )
--- src/components/ui/centredLayout/index.tsx
+++ src/components/ui/centredLayout/index.tsx
@@ -1,13 +1,9 @@
 import { ReactNode } from "react"
 
-const CentredLayout = ({children} : {children : ReactNode})=>{
-
-
-    return(
+export default function CenteredLayout({ children }: { children: ReactNode }) {
+    return (
         <div className="max-w-[var(--max-content-width)] mx-auto">
             {children}
         </div>
     )
 }
-
-export default CentredLayout
