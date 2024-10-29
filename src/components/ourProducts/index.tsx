eb7b13dccf36981d08801abd786c53557e855d00
aymkh <aymen4real@gmail.com>
2024-10-29 21:04:47 +0100

    chore: solution card restyling

--- src/components/ourProducts/index.tsx
+++ src/components/ourProducts/index.tsx
@@ -19,7 +19,8 @@ export default function ProductsSection() {
                 <section className="flex flex-col gap-10 ">
                     {
                         products.map((product: Products, index: number) => (
-                            <div key={index} className="relative flex flex-row w-full gap-6 bg-[#C7E6FF] rounded-lg">
+                            <div key={index} className="relative flex flex-row w-full gap-6 overflow-hidden productCard rounded-2xl group">
+                                <div className="absolute top-0 left-0 w-full h-56 productCardTopGrid"></div>
                                 <div className="flex flex-col items-start w-full gap-8 p-10">
                                     <div className="flex flex-col gap-2">
                                         <div className="flex flex-row items-center gap-4">
@@ -44,11 +45,11 @@ export default function ProductsSection() {
                                         {product.button}
                                     </Button>
                                 </div>
-                                <div className="flex items-end justify-end">
+                                <div className="flex flex-col justify-end">
                                     <Image
                                         src={product.preview}
                                         width={700}
-                                        className="w-full rounded-none"
+                                        className="w-full rounded-none group-hover:scale-110"
                                     />
                                 </div>
                             </div>
