7891fc6c3748f2752a302e61cea47a30f1f7da51
hellcat29A <bhiri22@gmail.com>
2024-10-29 17:11:57 +0100

    fix:hero-section-centred-layout

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -14,28 +14,31 @@ export default function HeroSection() {
     };
 
     return (
-        <CentredLayout>
-            <section id="hero-section" className="flex flex-row justify-between w-full gap-8 px-4 my-16 dark lg:p-0">
-                <div className="flex flex-col gap-8 ">
-                    <h1 className="max-w-2xl text-6xl tracking-tight text-left lg:px-0 light:text-black">
-                        {t('heroSection.h1')}
-                    </h1>
+        <section id="hero-section" className="">
 
-                    <p className="max-w-2xl text-lg font-light text-justify lg:text-left lg:px-0 light:text-gray-300">
-                        {t('heroSection.description')}
-                    </p>
+            <CentredLayout>
+                <div className="flex flex-row justify-between w-full gap-8 px-4 my-16 dark lg:p-0">
+                    <div className="flex flex-col gap-8 ">
+                        <h1 className="max-w-2xl text-6xl tracking-tight text-left lg:px-0 light:text-black">
+                            {t('heroSection.h1')}
+                        </h1>
 
-                    <div className="flex justify-center lg:justify-start">
+                        <p className="max-w-2xl text-lg font-light text-justify lg:text-left lg:px-0 light:text-gray-300">
+                            {t('heroSection.description')}
+                        </p>
 
-                        <Button variant="shadow" color="default" size="lg" className="w-fit" as={Link}
-                            onClick={() => { handleScroll() }}
-                        >
-                            {t('heroSection.button')}
-                        </Button>
+                        <div className="flex justify-center lg:justify-start">
+
+                            <Button variant="shadow" color="default" size="lg" className="w-fit" as={Link}
+                                onClick={() => { handleScroll() }}
+                            >
+                                {t('heroSection.button')}
+                            </Button>
+                        </div>
                     </div>
+                    <img src="images/hero-section/project_roadmap.png" className={`!-z-10 pointer-events-none hidden lg:block grayscale w-72 ${theme === 'dark' ? 'invert' : ''}`} />
                 </div>
-                <img src="images/hero-section/project_roadmap.png" className={`!-z-10 pointer-events-none hidden lg:block grayscale w-72 ${theme === 'dark' ? 'invert' : ''}`} />
-            </section>
-        </CentredLayout>
+            </CentredLayout>
+        </section>
     )
 }
