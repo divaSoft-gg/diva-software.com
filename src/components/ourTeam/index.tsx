18a41e8f6a3045300a83d65e6f2a4e315bd08d2d
hellcat29A <bhiri22@gmail.com>
2024-10-30 13:31:16 +0100

    fix:image-size-and-overlay

--- src/components/ourTeam/index.tsx
+++ src/components/ourTeam/index.tsx
@@ -18,14 +18,14 @@ export default function OurTeam() {
                         <div className="grid gap-4 p-4 responsiveGrid">
                             {
                                 element.data.map((item) => (
-                                    <div className="relative w-full group h-fit">
+                                    <div className="relative w-full h-fit">
                                         <Image
-                                            className="object-cover object-center w-full h-full transition duration-300 ease-in-out rounded-lg -z-10 group-hover:blur-sm"
+                                            className="object-cover object-center w-full h-full transition duration-300 ease-in-out rounded-lg -z-10"
                                             src={item.imagePath}
                                             alt={`${item.name} Image`}
                                         />
-                                        <div className="absolute inset-0 flex items-end justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                                            <div className="w-full p-2 text-center text-white bg-black bg-opacity-50 rounded-b-lg">
+                                        <div className="absolute inset-0 flex items-end justify-center transition-opacity duration-300 opacity-100 ">
+                                            <div className="w-full p-1 text-center text-white bg-black bg-opacity-50 rounded-b-lg h-fit">
                                                 <h4 className="text-lg font-bold">{item.name}</h4>
                                                 <small className="text-sm">{item.jobTitle}</small>
                                             </div>
